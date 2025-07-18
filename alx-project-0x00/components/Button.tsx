71eedafe31dcc8fb37a5d2614f9a902fc175bd40
Anita Samuel <migist2@gmail.com>
2025-07-18 11:37:42 +0100

    advanced

---
+++ alx-project-0x00/components/Button.tsx
@@ -0,0 +1,37 @@
+import { ButtonProps } from "@/interfaces";
+
+const Button: React.FC<ButtonProps> = ({
+  title,
+  size = "medium",
+  shape = "rounded-md",
+  className = '',
+  onClick
+}) => {
+  const sizeClasses = {
+    small: "px-3 py-1 text-sm",
+    medium: "px-4 py-2 text-base",
+    large: "px-6 py-3 text-lg",
+  };
+
+  const shapeClasses = {
+    "rounded-sm": "rounded-sm",
+    "rounded-md": "rounded-md",
+    "rounded-full": "rounded-full",
+  };
+
+  const baseClasses =
+    "border bg-blue-500 text-white font-medium hover:bg-blue-600 transition-colors cursor-pointer";
+
+const buttonClasses = `${baseClasses} ${sizeClasses[size]} ${shapeClasses[shape]} ${className}`;
+
+  return (
+    <button 
+      className={buttonClasses}
+      onClick={onClick}
+    >
+      {title}
+    </button>
+  );
+};
+
+export default Button
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,3 +1,4 @@
+import Button from "@/components/Button";
 import Card from "@/components/Card";
 
 const Landing: React.FC = () => {
@@ -5,6 +6,17 @@ const Landing: React.FC = () => {
         <div>
             <h1 className="text-xl font-extraLight">Landing Page</h1>
             <Card />
+
+<Button title="Click Me" />
+
+
+<Button title="Small Button" size="small" shape="rounded-full" />
+
+<Button 
+  title="Custom Button" 
+  className="bg-green-500 hover:bg-green-600" 
+  onClick={() => console.log('Clicked!')}
+/>
         </div>
     )
 }
