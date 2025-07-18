5d711620ae7252112196aadde6a1ca57e215edf0
Anita Samuel <migist2@gmail.com>
2025-07-18 09:20:37 +0100

    basic routing

---
+++ alx-project-0x00/pages/landing.tsx
@@ -0,0 +1,9 @@
+const Landing: React.FC = () => {
+    return(
+        <div>
+            <h1 className="text-xl font-extraLight">Landing Page</h1>
+        </div>
+    )
+}
+
+export default Landing;
